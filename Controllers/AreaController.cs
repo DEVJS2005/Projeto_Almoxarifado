cbb69ddf71d3ecd13c781644f1b618afdd7b35d0
DEVJS2005 <jvl14533@outlook.com>
2022-08-27 14:42:54 -0300

    Estrutura do projeto

---
+++ Controllers/AreaController.cs
@@ -0,0 +1,34 @@
+﻿using Almoxarifado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Almoxarifado.Controllers
+{
+    public class AreaController : Controller
+    {
+        BDAlmoxarifadoEntities db = new BDAlmoxarifadoEntities();
+        // GET: Area
+        public ActionResult areaIndex()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult areaCadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult areaCadastro(string areaDescricao)
+        {
+            Area area = new Area();
+            area.areaDescricao = areaDescricao;
+            db.Area.Add(area);
+            db.SaveChanges();
+            return View(db.Area.ToList());
+        }
+    }
+}
